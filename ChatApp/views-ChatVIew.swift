d63a511da05eaebe772e913b0f5a1906a14e307d
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-12-28 10:14:48 +0700

    Nothing

---
+++ ChatApp/views-ChatVIew.swift
@@ -0,0 +1,21 @@
+//
+//  views-ChatView.swift
+//  ChatApp
+//
+//  Created by Teneocto on 12/28/20.
+//  Copyright © 2020 NguyenCaoThiem. All rights reserved.
+//
+
+import SwiftUI
+
+struct views_ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct views_ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        views_ChatView()
+    }
+}
---
+++ ChatApp/views-MessageView.swift
@@ -0,0 +1,21 @@
+//
+//  views-MessageView.swift
+//  ChatApp
+//
+//  Created by Teneocto on 12/28/20.
+//  Copyright © 2020 NguyenCaoThiem. All rights reserved.
+//
+
+import SwiftUI
+
+struct views_MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct views_MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        views_MessageView()
+    }
+}
